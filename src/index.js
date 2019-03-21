765e10ff694b5e43f728d51162b2331ee58bfd83
ivanoff0a <ivanov-andrey15@mail.ru>
2019-03-21 21:05:49 +0300

    add a lot

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import { render } from 'react-dom';
+import React from 'react'
+import { render } from 'react-dom'
 import configureStore from './store/configureStore'
 import { Provider } from 'react-redux'
-import './index.css';
-import App from './App';
+import './index.css'
+import App from './containers/App'
 
 const store = configureStore()
 
